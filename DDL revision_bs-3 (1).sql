f2452c83888db69e81e250f0cafa9d464b23dbb7
jloc8749 <jloc8749@uni.sydney.edu.au>
2016-04-14 12:36:46 +1000

    fixed up the ddl file and added s to transaction in ER

--- DDL revision_bs-3 (1).sql
+++ DDL revision_bs-3 (1).sql
@@ -12,28 +12,29 @@
 clear screen;
 
 
-CREATE TABLE Participating_countries (
-  iso varchar (2),--I thought this was a two character code ? -jl -- fixed BS
+CREATE TABLE Participating_countries(
+  iso char (2),--I thought this was a two character code ? -jl -- fixed BS
   country_name varchar(20),--name spelling error -> country_name -jl
   PRIMARY KEY (iso)
 );
 
-CREATE TABLE Spectator (
-  id_type varchar(20),
+CREATE TABLE Spectator(
+  id_type varchar(22),
   country varchar(45),
-  o_state varchar(20),--i note you've used o_state where state not available -> need to update diagrams -jl
+  o_state char(10),--i note you've used o_state where state not available -> need to update diagrams -jl
   doc_id integer(15),
   address varchar(100),
   full_name varchar(70),
-  PRIMARY KEY (id_type, country, o_state, doc_id)
-);
+  PRIMARY KEY (id_type, country, o_state, doc_id));
 
 CREATE TABLE Event (
   event_name varchar (20),
-  round_no varchar(20),
+  round_no INTEGER(3),
   --3 char not enough ->20, i.e "AU, BR, US" are all competing in the event -jl
-  participating_countries varchar(20),
+  participating_countries char(2),--Wrong, this should have been put in the PK originally so that 
+  --we can have more than one country competing from the iso table. changed data type to iso standard -jl
   date_and_time datetime, --see: https://docs.oracle.com/cd/E17952_01/refman-5.5-en/datetime.html
+  --datetime forat: 'YYYY-MM-DD HH:MM:SS'
   --note I renamed "date	+	time" date_and_time
   sport varchar(20),
   PRIMARY KEY (event_name, round_no),--added comma -jl
@@ -54,8 +55,6 @@ CREATE TABLE Seat (
   aisle VARCHAR(2),--perhaps we could pull a seating chart from somewhere so we are clear on the suitable inputs to this table -jl
   gate CHAR,
   seat_class VARCHAR(20),
-  --added seat_class to key as it is in a disjoint ISA relationship with forced participation -jl
-  --added venue_name to PK. My opinion is that multiple seat classes for the same seat (at the same venue) are useless. -jl
   PRIMARY KEY (seat_code, seat_class, venue_name),
   FOREIGN KEY (venue_name) REFERENCES Venue (venue_name)
 );
@@ -69,7 +68,7 @@ CREATE TABLE Ticket (
   event_name VARCHAR(20),
   --name round not available --> round_no -jl
   --lets also discuss the data type for round_no next meeting -jl
-  round_no VARCHAR(20),
+  round_no INTEGER(3),--shoubld not be varchar here! -jl
   venue_name VARCHAR(20),
   PRIMARY KEY (barcode),
   FOREIGN KEY (seat_code, seat_class, venue_name) REFERENCES Seat (seat_code, seat_class, venue_name),
@@ -85,24 +84,12 @@ CREATE TABLE Ticket_status (
 );
 
 CREATE TABLE Transactions (--changed name to Transactions as Transaction not available -jl
-  transaction_number varchar(20) unique NOT NULL, --bs added unique to make tickets_sold work
-  price 
-  id_type varchar(20),
-  country varchar(45),
-  o_state varchar(20),
-  doc_id varchar(15
-  --address varchar(100), --why is this here?? -jl
-  --full_name varchar(70), --why is this here?? -jl
-  --PK removed: more than one spectator possible per transaction if buying multiple tickets -jl
-  --main constraint is that the spectator exists before transactions are generated (weak entity transaction discriminated by spectator) -jl
-  --*fingers crossed* -jl
-  --PRIMARY KEY (transaction_number)
-  --Commented by BS after tutor feedback FOREIGN KEY (id_type, country, o_state, doc_id) REFERENCES Spectator (id_type, country, o_state, doc_id)--removed comma -jl
-);
-
-CREATE TABLE Tickets_sold(
-  transaction_number varchar(20),
-  barcode INTEGER NOT NULL,--added not null -jl
-  FOREIGN KEY (transaction_number) REFERENCES Transactions (transaction_number),--added comma -jl
+  transaction_number integer(20) NOT NULL, --bs added unique to make tickets_sold work
+  --took out unique:tickets sold is not a table anymore and multiple tickets may be purchased in one transaction. -jl
+  --Transaction number is a NUMBER so I changed the data type to a number! -jl
+  price integer(4),--added a data type to price -jl
+  barcode integer
   FOREIGN KEY (barcode) REFERENCES Ticket (barcode)
 );
+
+--removed tickets sold table as per ER model changes -jl
